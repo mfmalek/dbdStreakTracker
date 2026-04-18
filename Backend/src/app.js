50ec92ee20e112af1d75ccef8a6707a4964be4aa
Malek <marcusfmalek@hotmail.com>
2026-04-18 15:42:02 -0300

    Fix: Added error middleware to app file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 
 const authRoutes = require("./modules/auth/auth.routes");
 const survivorRoutes = require("./modules/survivors/survivors.routes");
@@ -7,22 +8,21 @@ const groupRoutes = require("./modules/groups/groups.routes");
 const presetsRoutes = require("./modules/presets/presets.routes");
 const matchesRoutes = require('./modules/matches/matches.routes');
 const streakRoutes = require("./modules/streak/streak.routes");
-
-const app = express();
-
-app.use(cors({origin: "*",}));
-app.use(express.json());
+const errorMiddleware = require("./middlewares/error.middleware");
 
 app.get("/api/health", (req, res) => {
     console.log("Health check ping");
     res.status(200).json({ status: "ok" });
 });
 
+app.use(cors({origin: "*",}));
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/survivors", survivorRoutes);
 app.use("/api/groups", groupRoutes);
 app.use("/api/presets", presetsRoutes);
 app.use('/api/matches', matchesRoutes);
 app.use("/api/streak", streakRoutes);
+app.use(errorMiddleware);
 
 module.exports = app;
