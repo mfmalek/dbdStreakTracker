8e52d50ea494bfc08355a5e3f0b079967861ec4f
Malek <marcusfmalek@hotmail.com>
2026-05-28 23:14:08 -0300

    Fix: Reverted match creation on controller to previous structure

--- Backend/src/modules/matches/matches.controller.js
+++ Backend/src/modules/matches/matches.controller.js
@@ -9,7 +9,7 @@ const getMatches = async (req, res) => {
 
 const createMatch = async (req, res) => {
     const user = req.user.username;
-    const { mode, role, killerName, groupId, ...matchData } = req.validatedBody;
+    const { mode, role, killerName, groupId, ...matchData } = { ...req.body, ...req.validatedData};
     const newMatch = await matchesService.createMatch({
         user,
         mode,
