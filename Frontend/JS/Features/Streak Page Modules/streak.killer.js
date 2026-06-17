c4714bbe64a6251de55144da55bbe84d8bc83d30
Malek <marcusfmalek@hotmail.com>
2026-06-17 17:53:39 -0300

    Feat: Added timoeout on killer image page load

--- Frontend/JS/Features/Streak Page Modules/streak.killer.js
+++ Frontend/JS/Features/Streak Page Modules/streak.killer.js
@@ -1,8 +1,8 @@
-import { killerUI } from "../../Features/UI/KillerUI/killer.ui.js";
-import { killerController } from "../../Features/Killer Streak/killer.controller.js";
+import { killerUI } from "../UI/KillerUI/killer.ui.js";
+import { killerController } from "../Killer Streak/killer.controller.js";
 import { killerCore } from "../../Core/Streak/killer.core.js";
-import { killerListeners } from "../../Features/Killer Streak/killer.listeners.js";
-import { killerPresets } from "../../Features/Killer Streak/killer.presets.js";
+import { killerListeners } from "../Killer Streak/killer.listeners.js";
+import { killerPresets } from "../Killer Streak/killer.presets.js";
 
 export async function initKillerStreak({ group, matches, killerName, actions }) {
     await killerUI.initUI(group);
@@ -14,13 +14,17 @@ export async function initKillerStreak({ group, matches, killerName, actions }) 
     killerCore.initKillerOnlyUI();
     killerPresets.initPresets();
 
-    const currentKiller = killerName || document.getElementById('killerSelect')?.value; 
-
-    killerUI.applyKillerToUI(currentKiller || ""); 
-
-    if (currentKiller) {
-        killerCore.updateKillerAddons(currentKiller);
-    }
-
     killerListeners.initListeners(actions);
+
+    setTimeout(() => {
+        const killerSelect = document.getElementById('killerSelect');
+        const dropdownValue = killerSelect?.tomselect ? killerSelect.tomselect.getValue() : killerSelect?.value;
+        const currentKiller = killerName || dropdownValue || "";
+
+        killerUI.applyKillerToUI(currentKiller); 
+
+        if (currentKiller) {
+            killerCore.updateKillerAddons(currentKiller);
+        }
+    }, 100);
 }
--- Frontend/JS/Features/Streak Page Modules/streak.survivor.js
+++ Frontend/JS/Features/Streak Page Modules/streak.survivor.js
@@ -1,8 +1,8 @@
-import { survivorUI } from "../../Features/UI/SurvivorUI/survivor.ui.js";
-import { survivorController } from "../../Features/Survivor Streak/survivor.controller.js";
+import { survivorUI } from "../UI/SurvivorUI/survivor.ui.js";
+import { survivorController } from "../Survivor Streak/survivor.controller.js";
 import { survivorCore } from "../../Core/Streak/survivor.core.js";
-import { survivorListeners } from "../../Features/Survivor Streak/survivor.listeners.js";
-import { survivorPresets } from "../../Features/Survivor Streak/survivor.presets.js";
+import { survivorListeners } from "../Survivor Streak/survivor.listeners.js";
+import { survivorPresets } from "../Survivor Streak/survivor.presets.js";
 import { groupsApi } from "../../API/groups.api.js";
 
 export async function initSurvivorStreak({ group, matches, actions }) {
